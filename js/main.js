7b9644f9a993ec0cd99628a2c5cfe7cbc402214f
jkunwar <jkunwar@jkunwars-MacBook-Pro.local>
2022-05-18 22:22:05 -0400

    fix service worker issue

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./service-worker.js', { scope: '/' })
+    navigator.serviceWorker.register('/pwa-todo/service-worker.js', { scope: '/pwa-todo/' })
         .then((res) => {
             console.log(res)
         })
